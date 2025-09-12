61005cbecd9b8e1921331fc35618a931242ab284
ph-level-14 <haaspeter2009@gmail.com>
2025-09-12 11:02:31 +0200

    Creating loadQuizzes for loading the quizzes creating quizPath to get the Path of the quiz creating safe to get a safe name for the file

--- src/QuickStore.java
+++ src/QuickStore.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,6 +8,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+
+import static java.util.Collections.replaceAll;
 
 public class QuickStore {
     private static final Path QUIZ_DIR = Paths.get("quizzes");
@@ -26,6 +29,21 @@ public class QuickStore {
             Collections.sort(names);
             return names;
         }
+    }
+    public static List<String>loadquizzes(String name) throws IOException {
+        Path file = quizPath(name);
+        if(!Files.exists(file)){
+            return new ArrayList<>();
+        }
+        return Files.readAllLines(file, StandardCharsets.UTF_8);
+    }
+
+    public static Path quizPath(String name) throws IOException {
+        return QUIZ_DIR.resolve(safe(name) + ".txt");
+    }
+
+    public static String safe(String name) {
+        return name.replaceAll("[^a-zA-Z0-9-_]","_");
     }
 
     private static String striptExt(String fn) {
